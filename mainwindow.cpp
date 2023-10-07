8ca2d97b64f31bd37650c492bd138f54b7ce29f0
Vatosoa <razafindrazakav@gmail.com>
2023-10-07 11:28:20 +0300

    for windows & linux

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -42,11 +42,11 @@ void MainWindow::on_lineEdit_masque_cursorPositionChanged(int arg1, int arg2)
         QMessageBox::information(this,"message","classe C");
         ui->lineEdit_masque->setText("255.255.255.0");
     }
-    else if(ip.contains("169.")){
+    else if(ip.contains("172.")){
         QMessageBox::information(this,"message","classe B");
         ui->lineEdit_masque->setText("255.255.0.0");
     }
-    else if(ip.contains("169.")){
+    else if(ip.contains("10.")){
         QMessageBox::information(this,"message","classe A");
         ui->lineEdit_masque->setText("255.0.0.0");
     }
@@ -55,7 +55,11 @@ void MainWindow::on_lineEdit_masque_cursorPositionChanged(int arg1, int arg2)
 void MainWindow::on_btnValider_clicked()
 {
     if(ui->DHCP->isChecked() && ui->lineEdit_ip->text().isEmpty() && ui->lineEdit_masque->text().isEmpty()){
+#ifdef Q_OS_WIN // Vérifie si le système d'exploitation est Windows
         QString cmd("netsh interface ip set address \"Ethernet\" dhcp");
+#elif defined(Q_OS_LINUX) // Vérifie si le système d'exploitation est Linux
+        QString cmd("sudo ifconfig eth0 dhcp"); // Assurez-vous de remplacer "eth0" par l'interface correcte
+#endif
         QProcess proc;
         proc.start(cmd);
         proc.waitForFinished();
@@ -66,16 +70,24 @@ void MainWindow::on_btnValider_clicked()
         QMessageBox::information(this,"message","L'un des champs est vide.");
     }
     else {
+#ifdef Q_OS_WIN
         QString cmdIP("netsh interface ip set address \"Ethernet\" static " + ui->lineEdit_ip->text() + " " + ui->lineEdit_masque->text() + " " + ui->lineEdit_3->text() + " ");
         QString cmdDNS("netsh interface ip set dns \"Ethernet\" static " + ui->lineEdit_4->text() + " primary");
+#elif defined(Q_OS_LINUX)
+        QString cmdIP("sudo ifconfig eth0 " + ui->lineEdit_ip->text() + " netmask " + ui->lineEdit_masque->text());
+        QString cmdDNS("sudo echo 'nameserver " + ui->lineEdit_4->text() + "' > /etc/resolv.conf");
+#endif
         QProcess proc;
         proc.start(cmdIP);
+        proc.waitForFinished();
+        qDebug() << proc.readAll();
         proc.start(cmdDNS);
         proc.waitForFinished();
         qDebug() << proc.readAll();
         QMessageBox::information(this,"message","Commande réussie");
     }
 }
+
 
 void MainWindow::on_btnAnnuler_clicked()
 {
